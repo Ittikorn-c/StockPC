58413cbf2106ed503ad6c5ebd6a468bb3c188708
Unknown <ittikorn.c@ku.th>
2018-06-04 01:05:52 +0700

    Customer database
    add Customer database

---
+++ database/migrations/2018_06_03_171338_create_customer_phones_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerPhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_phones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('customer_id');
+            $table->string('phone_number', 10);
+            $table->timestamps();
+
+            $table->foreign('customer_id')
+                  ->references('id')
+                  ->on('Customer')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_phones');
+    }
+}
